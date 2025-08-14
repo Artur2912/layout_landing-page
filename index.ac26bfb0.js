4226c60b488d5bfc7d6416e4bfc786d8f285afb8
art <art@debian.art>
2025-08-14 20:48:26 +0300

    make build

---
+++ index.ac26bfb0.js
@@ -0,0 +1,2 @@
+document.querySelectorAll("textarea").forEach(function(t){t.style.height="auto",t.style.height=t.scrollHeight+"px",t.addEventListener("input",function(){t.style.height="auto",t.style.height=t.scrollHeight+"px"})});
+//# sourceMappingURL=index.ac26bfb0.js.map
